6120f1e933113c29082598bfec3e31b608358c64
Nathan <102113528@student.swin.edu.au>
2021-04-30 23:45:46 +1000

    Implement the event handler system

---
+++ MicrowaveGame2D/Assets/Scripts/Events/EventId.cs
@@ -0,0 +1,15 @@
+﻿namespace Events
+{
+    public readonly struct EventId
+    {
+        private readonly ulong _value;
+
+        private EventId(ulong value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator EventId(ulong value) => new EventId(value);
+        public static implicit operator ulong(EventId value) => value._value;
+    }
+}
