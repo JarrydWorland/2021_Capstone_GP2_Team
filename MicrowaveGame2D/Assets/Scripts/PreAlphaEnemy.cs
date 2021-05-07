bcddf52b22e6b673879bfd9bb6add8f11e6ee9aa
Kieran Freame <62127214+102109190@users.noreply.github.com>
2021-05-07 12:04:28 +1000

    Added Additional NavMeshComponents

--- MicrowaveGame2D/Assets/Scripts/PreAlphaEnemy.cs
+++ MicrowaveGame2D/Assets/Scripts/PreAlphaEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pre-Alpha-Enemy : MonoBehaviour
+public class PreAlphaEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
