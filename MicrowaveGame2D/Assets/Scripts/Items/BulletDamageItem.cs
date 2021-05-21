500f053cfd11b45815c55703ea44855885dd1dba
BasilChristofis <42164170+BasilChristofis@users.noreply.github.com>
2021-05-21 13:28:55 +1000

    More Upgrades(Passive)
    Added Bullet Speed, Bullet Rate, and Bullet Velocity Passive Items to the game. Not Functional right now.

---
+++ MicrowaveGame2D/Assets/Scripts/Items/BulletDamageItem.cs
@@ -0,0 +1,43 @@
+using Player;
+using UnityEngine;
+
+namespace Items
+{
+	public class BulletDamageItem : BaseItem
+	{
+		public float IncreaseValue, DurationValue;
+
+		private PlayerMovement _playerMovement;
+		private float _time;
+
+		private bool _used;
+		public override bool Used => _used;
+
+		private void Start()
+		{
+			_playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+		}
+
+		public override void Use()
+		{
+			if (_used) return;
+            //_playerMovement.Speed += IncreaseValue;
+            //_used = true;
+        }
+
+        public override void ItemUpdate()
+		{
+			//if (!_used) return;
+			_playerMovement.Speed += IncreaseValue;
+			_used = true;
+
+			_time += Time.deltaTime;
+			if (_time >= DurationValue) Destroy(gameObject);
+		}
+
+		private void OnDestroy()
+		{
+			_playerMovement.Speed -= IncreaseValue;
+		}
+	}
+}
---
+++ MicrowaveGame2D/Assets/Scripts/Items/WeaponsFireRateItem.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Items
+{
+	public class WeaponsFireRateItem : BaseItem
+	{
+		public int IncreaseValue;
+
+		private bool _used;
+		public override bool Used => _used;
+
+		public override void Use()
+		{
+			if (_used) return;
+
+			GameObject playerObject = GameObject.Find("Player");
+			Health playerHealth = playerObject.GetComponent<Health>();
+
+			if (playerHealth.Value < playerHealth.MaxHealth)
+			{
+				playerHealth.Value += IncreaseValue;
+				_used = true;
+
+				Destroy(gameObject);
+			}
+		}
+
+		public override void ItemUpdate() { }
+	}
+}
