0610003e05470980c57c17f9485e1bda06dbd909
Kieran Freame <102109190@student.swin.edu.au>
2021-06-02 17:22:07 +1000

    Ending Narrative Panel
    Added Ending Narrative panel
    Updated ChangeText to accommodate more than one passage
    Change NarrativeButton to BeginningNarrativeButton
    Added EndNarrativeButton script

---
+++ MicrowaveGame2D/Assets/Scripts/EndNarrativeButton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndNarrativeButton : MonoBehaviour
+{
+    public ChangeText narrativeTxt;
+    [SerializeField] private Text btnTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnTxt.GetComponent<Text>().text = "Continue";
+        transform.parent.gameObject.SetActive(false);
+    }
+
+    public void SwapText()
+    {
+        if (btnTxt.GetComponent<Text>().text == "Return to Main Menu")
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        else
+        {
+            narrativeTxt.SwapText();
+        }
+
+        if (narrativeTxt.AtEnd())
+        {
+            btnTxt.GetComponent<Text>().text = "Return to Main Menu";
+            return;
+        } 
+    }
+}
