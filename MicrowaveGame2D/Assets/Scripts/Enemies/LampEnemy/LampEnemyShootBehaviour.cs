2d403c1ad946788ade916128b9b9ed8fc92ff05b
Andrew <102107466@student.swin.edu.au>
2021-06-03 02:09:06 +1000

    Added turret animations

--- MicrowaveGame2D/Assets/Scripts/Enemies/LampEnemy/LampEnemyShootBehaviour.cs
+++ MicrowaveGame2D/Assets/Scripts/Enemies/LampEnemy/LampEnemyShootBehaviour.cs
@@ -3,16 +3,19 @@ using Weapons;
 
 namespace Enemy.Turret
 {
-	public class TurretShootBehaviour : MonoBehaviour
+	[RequireComponent(typeof(Animator))]
+	public class LampEnemyShootBehaviour : MonoBehaviour
 	{
 		[Tooltip("Weapon to shoot with")]
 		public BaseWeapon Weapon;
 
 		private GameObject _target;
+		private Animator _animator;
 
 		private void Start()
 		{
 			_target = GameObject.Find("Player");
+			_animator = GetComponent<Animator>();
 			Weapon.Shoot();
 		}
 
@@ -21,6 +24,9 @@ namespace Enemy.Turret
 			if (_target != null)
 			{
 				Weapon.Direction = _target.transform.position - transform.position;
+				_animator.SetFloat("DirectionX", Weapon.Direction.x);
+				_animator.SetFloat("DirectionY", Weapon.Direction.y);
+				_animator.SetFloat("FireRate", Weapon.FireRate);
 			}
 		}
 	}
