3be96ed7dcfd6d7dd40a155c5c62d28dc768d2c8
Andrew <102107466@student.swin.edu.au>
2021-06-04 01:37:58 +1000

    Added room clear callback

--- MicrowaveGame2D/Assets/Scripts/Level/Room.cs
+++ MicrowaveGame2D/Assets/Scripts/Level/Room.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Events;
+using Enemy;
 
 namespace Level
 {
@@ -8,8 +10,9 @@ namespace Level
 	{
 		public IEnumerable<Door> Doors => GetComponentsInChildren<Door>(true);
 		public IEnumerable<DoorDirection> Directions => Doors.Select(door => door.Direction);
-
 		public Vector2Int Position { get; private set; }
+		private int _enemiesInRoom;
+		private EventId<HealthChangedEventArgs> _healthChangedEventId;
 
 		public bool HasDoorFacing(DoorDirection direction)
 		{
@@ -29,6 +32,44 @@ namespace Level
 			room.Position = position;
 
 			return room;
+		}
+
+		private void Start()
+		{
+			_enemiesInRoom = GetComponentsInChildren<EnemyHealthBehaviour>().Length;
+		}
+
+		private void OnHealthChangedEvent(HealthChangedEventArgs eventArgs)
+		{
+			bool isEnemy = eventArgs.GameObject.GetComponent<EnemyHealthBehaviour>() != null;
+			bool isDead = eventArgs.NewValue == 0;
+			bool isWithinRoom = eventArgs.GameObject.transform.parent == transform;
+
+			if (isEnemy && isDead && isWithinRoom)
+			{
+				_enemiesInRoom -= 1;
+				if (_enemiesInRoom <= 0)
+				{
+					OnRoomCleared();
+				}
+			}
+		}
+
+		private void OnRoomCleared()
+		{
+			print(name + " has been cleared!");
+		}
+
+		private void OnEnable()
+		{
+			// register the OnHealthChangedEvent whenever the room becomes active
+			_healthChangedEventId = EventManager.Register<HealthChangedEventArgs>(OnHealthChangedEvent);
+		}
+
+		private void OnDisable()
+		{
+			// unregister the OnHealthChangedEvent whenever the room becomes inactive
+			EventManager.Unregister(_healthChangedEventId);
 		}
 	}
 }
