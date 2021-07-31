32887f2fe232c97970092dfa2ddacbbdb8bc0a91
Nathan <102113528@student.swin.edu.au>
2021-08-01 09:31:33 +1000

    Implement death / game over menu

---
+++ MicrowaveGame/Assets/Resources/Scripts/Menus/MenuDeathBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Scripts.Menus
+{
+	public class MenuDeathBehaviour : MenuBehaviour
+	{
+		/// <summary>
+		/// Sets the current scene to the "Gameplay" scene.
+		/// Called when the "Try Again" button is pressed.
+		/// </summary>
+		public void OnAgainButtonPressed() => SceneManager.LoadScene("Gameplay");
+
+		/// <summary>
+		/// Sets the current scene to the "Menu" scene.
+		/// Called when the "Exit To Main Menu" button is pressed.
+		/// </summary>
+		public void OnExitButtonPressed() => SceneManager.LoadScene("Menu");
+	}
+}
---
+++ MicrowaveGame/Assets/Resources/Scripts/Player/PlayerDeathBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using Scripts.Events;
+using Scripts.Menus;
+using UnityEngine;
+
+namespace Scripts.Player
+{
+	public class PlayerDeathBehaviour : MonoBehaviour
+	{
+		private EventId<HealthChangedEventArgs> _healthChangedEventId;
+
+		private void Start()
+		{
+			_healthChangedEventId = EventManager.Register<HealthChangedEventArgs>(OnHealthChanged);
+		}
+
+		private void OnHealthChanged(HealthChangedEventArgs eventArgs)
+		{
+			if (eventArgs.NewValue == 0)
+			{
+				Time.timeScale = 0.0f;
+
+				// Surely there's a better way to find an inactive object...
+				MenuDeathBehaviour deathMenu =
+					transform.parent.transform.parent.Find("MenuDeath").GetComponent<MenuDeathBehaviour>();
+
+				MenuManager.GoInto(deathMenu);
+			}
+		}
+
+		private void OnDestroy() => EventManager.Unregister(_healthChangedEventId);
+	}
+}
