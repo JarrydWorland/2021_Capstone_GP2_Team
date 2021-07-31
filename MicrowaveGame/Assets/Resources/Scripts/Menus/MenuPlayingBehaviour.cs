637e1de4df47c13ef023fcb54ed6957a06701c42
Nathan <102113528@student.swin.edu.au>
2021-08-01 00:13:55 +1000

    Implement narrative menu

--- MicrowaveGame/Assets/Resources/Scripts/Menus/MenuPlayingBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Menus/MenuPlayingBehaviour.cs
@@ -5,12 +5,6 @@ namespace Scripts.Menus
 {
 	public class MenuPlayingBehaviour : MenuBehaviour
 	{
-		private void Start()
-		{
-			Time.timeScale = 1.0f;
-			MenuManager.Init(this);
-		}
-
 		/// <summary>
 		/// Called via Unity's new input system when the user presses the "Escape" key.
 		/// Sets the current menu to the "Controls" menu.
@@ -22,7 +16,7 @@ namespace Scripts.Menus
 			if (!context.performed) return;
 
 			Time.timeScale = 0.0f;
-			
+
 			MenuPausedBehaviour pausedMenu =
 				transform.parent.Find("MenuPaused").GetComponent<MenuPausedBehaviour>();
 
