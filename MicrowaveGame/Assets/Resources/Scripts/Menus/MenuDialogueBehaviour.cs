3a367de8bdcf21472f3d932c8ad3766a1dbc87ef
Andrew <102107466@student.swin.edu.au>
2021-08-22 23:28:52 +1000

    Added Hub

--- MicrowaveGame/Assets/Resources/Scripts/Menus/MenuDialogueBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Menus/MenuDialogueBehaviour.cs
@@ -14,9 +14,11 @@ namespace Scripts.Menus
 
 		private Text _continueButtonText;
 
-		private Queue<string> _sentences;
-		private Lerped<string> _currentSentence;
-		private int _maxSentenceLength;
+		private Queue<string> _sentences = new Queue<string>();
+		private Lerped<string> _currentSentence = new Lerped<string>(string.Empty, (1.0f / _charactersPerSecond) * _maxSentenceLength , Interpolate, true);
+		private const int _maxSentenceLength = 200;
+		private const float _charactersPerSecond = 30.0f;
+		private Action _onDialogueComplete = null;
 
 		public override void OnEnter() => MenuManager.Pause();
 		public override void OnLeave() => MenuManager.Resume();
@@ -26,10 +28,6 @@ namespace Scripts.Menus
 			_speakerText = GameObject.Find("SpeakerText").GetComponent<Text>();
 			_contentText = GameObject.Find("DialogueText").GetComponent<Text>();
 			_continueButtonText = GameObject.Find("ContinueButton").GetComponentInChildren<Text>();
-
-			_sentences = new Queue<string>();
-			_currentSentence = new Lerped<string>(string.Empty, 2.0f, Interpolate, true);
-			_maxSentenceLength = int.MinValue;
 		}
 
 		private void Update()
@@ -41,14 +39,14 @@ namespace Scripts.Menus
 		/// Given a dialogue object, load the sentences and display the first sentence.
 		/// </summary>
 		/// <param name="dialogue"></param>
-		public void StartDialogue(DialogueContent dialogue)
+		public void StartDialogue(DialogueContent dialogue, Action onDialogueComplete = null)
 		{
 			_speakerText.text = dialogue.Speaker;
 			_sentences.Clear();
+			_onDialogueComplete = onDialogueComplete;
 
 			foreach (string sentence in dialogue.Sentences)
 			{
-				if (sentence.Length > _maxSentenceLength) _maxSentenceLength = sentence.Length;
 				_sentences.Enqueue(sentence);
 			}
 
@@ -63,6 +61,7 @@ namespace Scripts.Menus
 			if (_sentences.Count == 0)
 			{
 				MenuManager.GoBack();
+				if (_onDialogueComplete != null) _onDialogueComplete();
 				return;
 			}
 
@@ -74,7 +73,7 @@ namespace Scripts.Menus
 		/// Interpolates between an empty string and the given full string.
 		/// Example: "" at 0.0f, "a" at 0.25f ... "abcd" at 1.0f.
 		/// </summary>
-		private string Interpolate(string _, string line, float interpolation)
+		private static string Interpolate(string _, string line, float interpolation)
 		{
 			if (string.IsNullOrWhiteSpace(line)) return string.Empty;
 
