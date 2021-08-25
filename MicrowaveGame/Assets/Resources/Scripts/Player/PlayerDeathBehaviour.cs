011b0aff81a7d63b61048c7102e33ec83c005cd7
BasilChristofis <42164170+BasilChristofis@users.noreply.github.com>
2021-08-25 19:23:04 +1000

    Particle Changes
    Added new edits to particles to shorten their durations and rate/lifetime. I also fixed some layering issues that interfered with how the particles appeared on screen, causing issues with visibility.

--- MicrowaveGame/Assets/Resources/Scripts/Player/PlayerDeathBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Player/PlayerDeathBehaviour.cs
@@ -9,9 +9,15 @@ namespace Scripts.Player
 	{
 		private EventId<HealthChangedEventArgs> _healthChangedEventId;
 
+		public ParticleSystem deathParticles;
+
 		private void Start()
 		{
 			_healthChangedEventId = EventManager.Register<HealthChangedEventArgs>(OnHealthChanged);
+			deathParticles = transform.GetChild(6).GetComponent<ParticleSystem>();
+			transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+			transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = true;
+			transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = true;
 		}
 
 		private void OnHealthChanged(HealthChangedEventArgs eventArgs)
@@ -19,6 +25,10 @@ namespace Scripts.Player
 			if (eventArgs.GameObject.name == "Player" && eventArgs.NewValue == 0)
 			{
 				Time.timeScale = 0.0f;
+				deathParticles.Play();
+				transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+				transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
+				transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = false;
 				GameObject.Find("Player").GetComponent<PlayerInput>().actions.Disable();
 				MenuManager.GoInto("MenuDeath");
 			}
