f969fbd0d1f8855786f886b55673e5aa20cd3a89
BasilChristofis <42164170+BasilChristofis@users.noreply.github.com>
2021-08-26 13:21:59 +1000

    Updated Beam Weapon Radius
    Changed the Radius of the BEam Weapon Projectiles to 2 units instead of 4 units.

--- MicrowaveGame/Assets/Resources/Scripts/Projectiles/ProjectileBeamBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Projectiles/ProjectileBeamBehaviour.cs
@@ -4,7 +4,7 @@ namespace Scripts.Projectiles
 {
 	public class ProjectileBeamBehaviour : ProjectileBehaviour
 	{
-		private float _radius;
+		private float _radius = 2;
 		//Designed to create a range around the player where he is shooting a Beam. 
 		//Currently it creates a range around the player only based on the transform.position of the spawn of the starting room.
 		protected override void Update()
@@ -12,7 +12,6 @@ namespace Scripts.Projectiles
 			base.Update();
 			GameObject Player = GameObject.Find("Player");
 			float distance = Vector3.Distance(Player.transform.position, transform.position);
-			_radius = 4;
 			transform.position += (Vector3)Direction;
 			if (distance > _radius) Destroy(gameObject);
 		}
