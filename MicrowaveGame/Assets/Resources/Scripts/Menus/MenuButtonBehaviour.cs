ca5cd75f0ea26641c0064735f62ee11c4eb51d1f
Nathan <102113528@student.swin.edu.au>
2021-09-17 16:57:23 +1000

    Update menu button design

--- MicrowaveGame/Assets/Resources/Scripts/Menus/MenuButtonBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Menus/MenuButtonBehaviour.cs
@@ -4,12 +4,34 @@ using UnityEngine.UI;
 
 namespace Scripts.Menus
 {
-	public class MenuButtonBehaviour : MonoBehaviour, IPointerEnterHandler
+	public class MenuButtonBehaviour : MonoBehaviour, IPointerEnterHandler, ISelectHandler
 	{
 		public void OnPointerEnter(PointerEventData eventData)
 		{
-			Selectable selectable = eventData.pointerEnter.GetComponentInParent<Selectable>();
-			if (selectable != null) selectable.Select();
+			Button button = eventData.pointerEnter.GetComponentInParent<Button>();
+			if (button != null) button.Select();
+		}
+
+		public void OnSelect(BaseEventData eventData)
+		{
+			Button button = eventData.selectedObject.GetComponent<Button>();
+			if (button == null) return;
+
+			// Enable the indicators for the selected button.
+			SetButtonState(button, true);
+
+			// Disable the indicators for the previously selected button.
+			if (MenuManager.Current.CurrentSelectable is Button previousButton && previousButton != button)
+				SetButtonState(previousButton, false);
+
+			// Update the currently selected button reference for the current menu.
+			MenuManager.Current.CurrentSelectable = button;
+		}
+
+		private void SetButtonState(Button button, bool state)
+		{
+			Image[] images = button.GetComponentsInChildren<Image>();
+			foreach (Image image in images) image.enabled = state;
 		}
 	}
 }
