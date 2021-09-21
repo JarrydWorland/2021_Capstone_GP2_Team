8836fdc97d90e97b1e9b15c89a6526c705ea1d7d
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-09-21 14:52:40 +1000

    CHANGE: Updated enemy to explode upon death with the new explosion particle system

--- MicrowaveGame/Assets/Resources/Scripts/Enemies/EnemyDeathBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Enemies/EnemyDeathBehaviour.cs
@@ -6,6 +6,7 @@ namespace Scripts.Enemies
 	class EnemyDeathBehaviour : MonoBehaviour
 	{
 		private HealthBehaviour _healthBehaviour;
+		public ParticleSystem deathParticle;
 
 		private void Start()
 		{
@@ -16,8 +17,25 @@ namespace Scripts.Enemies
 		{
 			if (_healthBehaviour.Value == 0)
 			{
+				Explode();
 				Destroy(gameObject);
 			}
+		}
+
+		void Explode()
+		{
+			//Instantiate our one-off particle system
+			ParticleSystem explosionEffect = Instantiate(deathParticle) as ParticleSystem;
+			Vector3 spawnPosition = new Vector3(transform.position.x,transform.position.y,transform.position.z-2);
+			explosionEffect.transform.position = spawnPosition;
+			
+			//play it
+			//explosionEffect.main.loop = false;
+			explosionEffect.Play();
+
+			//destroy the particle system when its duration is up, right
+			//it would play a second time.
+			Destroy(explosionEffect.gameObject, explosionEffect.main.duration);
 		}
 	}
 }
