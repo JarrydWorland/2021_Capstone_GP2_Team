74b408a1f27685617c51b6d04efd28a784a335f2
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-09-25 17:43:03 +1000

    CHANGE: Added a spark when being hit to the player and an explosion on death

--- MicrowaveGame/Assets/Resources/Scripts/Player/PlayerDeathBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Player/PlayerDeathBehaviour.cs
@@ -14,7 +14,6 @@ namespace Scripts.Player
 		private void Start()
 		{
 			_healthChangedEventId = EventManager.Register<HealthChangedEventArgs>(OnHealthChanged);
-			deathParticles = transform.GetChild(6).GetComponent<ParticleSystem>();
 			transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
 			transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = true;
 			transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = true;
@@ -25,7 +24,7 @@ namespace Scripts.Player
 			if (eventArgs.GameObject.name == "Player" && eventArgs.NewValue == 0)
 			{
 				Time.timeScale = 0.0f;
-				deathParticles.Play();
+				Explode();
 				transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
 				transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
 				transform.GetChild(2).GetComponent<SpriteRenderer>().enabled = false;
@@ -35,5 +34,19 @@ namespace Scripts.Player
 		}
 
 		private void OnDestroy() => EventManager.Unregister(_healthChangedEventId);
+
+		void Explode()
+		{
+			//Instantiate our one-off particle system
+			ParticleSystem explosionEffect = Instantiate(deathParticles) as ParticleSystem;
+			Vector3 spawnPosition = new Vector3(transform.position.x,transform.position.y,transform.position.z-2); //have to use -2 because sprite ordering is too difficult to implement this late in the project.
+			explosionEffect.transform.position = spawnPosition;
+			
+			//play it
+			explosionEffect.Play();
+
+			//destroy the particle system when its duration is up
+			Destroy(explosionEffect.gameObject, explosionEffect.main.duration);
+		}
 	}
 }
