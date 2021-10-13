a135c522f9742d0fd5d951c7cb4d25a793bafbd8
BasilChristofis <42164170+BasilChristofis@users.noreply.github.com>
2021-10-13 11:13:54 +1100

    Fixed Logic Bug
    Changed code style to match Spark method of implementation.
    Also changed Particle to not start on Awake.

--- MicrowaveGame/Assets/Resources/Scripts/Enemies/EnemyDeathBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Enemies/EnemyDeathBehaviour.cs
@@ -22,10 +22,10 @@ namespace Scripts.Enemies
 				Explode();
 				Destroy(gameObject);
 			}
-			if(_healthBehaviour.Value <= 2)
+			if(_healthBehaviour.Value == 2)
             {
-				Smoke();
-            }
+				_smokeParticle.Play();
+			}
 		}
 
 		void Explode()
@@ -40,19 +40,6 @@ namespace Scripts.Enemies
 
 			//destroy the particle system when its duration is up
 			Destroy(explosionEffect.gameObject, explosionEffect.main.duration);
-		}
-
-		void Smoke()
-		{
-			//Instantiate our one-off particle system
-			ParticleSystem smokeEffect = Instantiate(_smokeParticle) as ParticleSystem;
-			Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.1f); //have to use -2 because sprite ordering is too difficult to implement this late in the project.
-			smokeEffect.transform.position = spawnPosition;
-			//play it
-			smokeEffect.Play();
-
-			//destroy the particle system when its duration is up
-			Destroy(smokeEffect, smokeEffect.main.duration);
 		}
 	}
 }
