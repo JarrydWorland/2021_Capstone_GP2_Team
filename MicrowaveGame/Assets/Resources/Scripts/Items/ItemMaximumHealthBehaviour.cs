76870ec95eb2634039fc49fd222a69f4954bcc14
BasilChristofis <42164170+BasilChristofis@users.noreply.github.com>
2021-10-27 18:09:04 +1100

    realized I was working on wrong item
    Changed it to match theconsumable health update.

--- MicrowaveGame/Assets/Resources/Scripts/Items/ItemMaximumHealthBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Items/ItemMaximumHealthBehaviour.cs
@@ -11,18 +11,12 @@ namespace Scripts.Items
     {
         public int IncreaseValue;
 
-        private Color setColor = new Color(61, 255, 98, 255);
-        private Color makeColor;
+        private bool _isUsed;
 
         private HealthBehaviour _playerHealthBehaviour;
-        private HealthBarBehaviour healthBarBehaviour;
-        List<Animator> _cellAnimators = new List<Animator>();
-
-        public GameObject HealthBar;
-        public GameObject Cell;
 
         public AudioClip itemDrop;
-        private int _pMax;
+        public AudioClip healthSFX;
 
         public override void Start()
         {
@@ -30,46 +24,32 @@ namespace Scripts.Items
 
             Description = string.Format(Description, IncreaseValue);
             _playerHealthBehaviour = GameObject.Find("Player").GetComponent<HealthBehaviour>();
-            healthBarBehaviour = GameObject.Find("HealthBar").GetComponent<HealthBarBehaviour>();
-            _pMax = _playerHealthBehaviour.MaxHealth;
         }
 
         public override void OnPickupItem(InventorySlotBehaviour inventorySlotBehaviour)
         {
-            _playerHealthBehaviour.MaxHealth += IncreaseValue;
-            _playerHealthBehaviour.Value = _playerHealthBehaviour.MaxHealth;
-            healthBarBehaviour.Target = GameObject.Find("Player");
-            CreateCells();
-            inventorySlotBehaviour.PlayAnimation("InventorySlotBounceLoop");
+            inventorySlotBehaviour.PlayAnimation("InventorySlotBounceExpand");
         }
 
-        public override void OnUseItem(InventorySlotBehaviour inventorySlotBehaviour) { }
+        public override void OnUseItem(InventorySlotBehaviour inventorySlotBehaviour) {
+            _isUsed = true;
+
+            _playerHealthBehaviour.Value = _playerHealthBehaviour.MaxHealth;
+            Debug.Log($"{_playerHealthBehaviour.transform.name}'s Health Reset to Max");
+            AudioManager.Play(healthSFX, 0.75f, false);
+            Destroy(gameObject);
+        }
 
         public override void OnUpdateItem(InventorySlotBehaviour inventorySlotBehaviour) { }
 
         public override bool OnDropItem(InventorySlotBehaviour inventorySlotBehaviour)
         {
-            _playerHealthBehaviour.MaxHealth -= IncreaseValue;
-            _playerHealthBehaviour.Value = _playerHealthBehaviour.MaxHealth;
-            Destroy(Cell);
+            if(!_isUsed)
+            {
             inventorySlotBehaviour.PlayAnimation("InventorySlotBounceContract");
             AudioManager.Play(itemDrop, 0.55f);
-            return true;
-        }
-
-        private void CreateCells()
-        {
-            const float margin = 0.9f;
-            float cellHeight = Cell.GetComponent<SpriteRenderer>().bounds.size.y;
-            Cell.GetComponent<SpriteRenderer>().color = setColor;
-            for (int i = _pMax; i < _playerHealthBehaviour.MaxHealth; i++)
-            {
-                HealthBar = Instantiate(Cell, healthBarBehaviour.transform);
-                HealthBar.transform.position += new Vector3((cellHeight * margin * i), 0, 0);
-                makeColor = Cell.GetComponent<SpriteRenderer>().color;
-                Cell.GetComponent<SpriteRenderer>().color = setColor;
-                _cellAnimators.Add(HealthBar.GetComponent<Animator>());
             }
+            return true;
         }
     }
 }
