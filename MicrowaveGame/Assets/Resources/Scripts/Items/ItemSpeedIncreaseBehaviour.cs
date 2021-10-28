3e34c15591ad693a7e95786d007d1e3ff20d7dd2
Andrew <102107466@student.swin.edu.au>
2021-10-28 12:09:59 +1100

    Removed unused code

--- MicrowaveGame/Assets/Resources/Scripts/Items/ItemSpeedIncreaseBehaviour.cs
+++ MicrowaveGame/Assets/Resources/Scripts/Items/ItemSpeedIncreaseBehaviour.cs
@@ -1,6 +1,5 @@
 using Scripts.Inventory;
 using Scripts.Player;
-using Scripts.StatusEffects;
 using UnityEngine;
 using Scripts.Audio;
 
@@ -13,23 +12,15 @@ namespace Scripts.Items
 		/// </summary>
 		public float IncreaseValue;
 
-		/// <summary>
-		/// The duration of the item.
-		/// </summary>
-		//public int DurationValue;
+		public AudioClip ItemDrop;
 
-		//private bool _isActive, _isUsed;
 		private float _time;
 
 		private PlayerMovementBehaviour _playerMovementBehaviour;
 
-		public AudioClip itemDrop;
-		public AudioClip speedIncrease;
-
 		public override void Start()
 		{
 			base.Start();
-			Description = string.Format(Description, IncreaseValue * 100.0f);
 			_playerMovementBehaviour = GameObject.Find("Player").GetComponent<PlayerMovementBehaviour>();
 		}
 
@@ -38,38 +29,16 @@ namespace Scripts.Items
 			_playerMovementBehaviour.MaxVelocity += IncreaseValue;
 			inventorySlotBehaviour.PlayAnimation("InventorySlotBounceExpand");
 		}
-		public override void OnUseItem(InventorySlotBehaviour inventorySlotBehaviour)
-		{
-			//if (_isActive) return;
-			//_isActive = _isUsed = true;
 
-			//GameObject.Find("Player").GetComponent<StatusEffectBehaviour>()
-			//	.Apply<StatusEffectFaster>(DurationValue, IncreaseValue);
+		public override void OnUseItem(InventorySlotBehaviour inventorySlotBehaviour) { }
 
-			//AudioManager.Play(speedIncrease, 0.75f, false);
-
-			//inventorySlotBehaviour.PlayAnimation("InventorySlotBounceLoop");
-		}
-
-		public override void OnUpdateItem(InventorySlotBehaviour inventorySlotBehaviour)
-		{
-			//if (!_isActive) return;
-
-			//_time += Time.deltaTime;
-			//if (_time < DurationValue) return;
-
-			//_isActive = false;
-
-			//inventorySlotBehaviour.PlayAnimation("InventorySlotBounceContract");
-			//inventorySlotBehaviour.DropItem();
-			//Destroy(gameObject);
-		}
+		public override void OnUpdateItem(InventorySlotBehaviour inventorySlotBehaviour) { }
 
 		public override bool OnDropItem(InventorySlotBehaviour inventorySlotBehaviour)
 		{
 			_playerMovementBehaviour.MaxVelocity -= IncreaseValue;
 			inventorySlotBehaviour.PlayAnimation("InventorySlotBounceContract");
-			AudioManager.Play(itemDrop, 0.55f);
+			AudioManager.Play(ItemDrop, 0.55f);
 			return true;
 		}
 	}
